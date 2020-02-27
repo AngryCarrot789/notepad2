4ec8ade189e25c1753a10e079a674387aac3e091
jbbbw <kettlesimzulator420@gmail.com>
2020-02-27 18:56:50 +0000

    AAAAAAAAAAAAAAAAAAAAAAAAAAAA (ignore)

--- Notepad2/MainWindow.xaml.cs
+++ Notepad2/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ namespace Notepad2
     /// </summary>
     public partial class MainWindow : Window
     {
+        //hannez u r a google
+        //(ignore this if you aren't hannez)
         public bool DarkThemeEnabled { get; set; }
         public App CurrentApp;
         public MainViewModel ViewModel { get; set; }
