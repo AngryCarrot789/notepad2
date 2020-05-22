6fec414735914cbb83c77c469c18e85c3de1ee6d
Александр Мохов <u1035@mail.ru>
2020-05-22 22:58:45 +0300

    Small cleanup in Format Model and TextDecorationCollection initialization

--- Notepad2/Notepad/FormatModel.cs
+++ Notepad2/Notepad/FormatModel.cs
@@ -9,18 +9,18 @@ namespace Notepad2.Notepad
         private FontStyle _style;
         public FontStyle Style
         {
-            get { return _style; }
-            set { RaisePropertyChanged(ref _style, value); }
+            get => _style;
+            set => RaisePropertyChanged(ref _style, value);
         }
 
         private FontWeight _weight;
         public FontWeight Weight
         {
-            get { return _weight; }
-            set { RaisePropertyChanged(ref _weight, value); }
+            get => _weight;
+            set => RaisePropertyChanged(ref _weight, value);
         }
 
-        private TextDecorationCollection _decoration;
+        private TextDecorationCollection _decoration = new TextDecorationCollection();
         public TextDecorationCollection Decoration
         {
             get => _decoration;
@@ -30,24 +30,21 @@ namespace Notepad2.Notepad
         private FontFamily _family;
         public FontFamily Family
         {
-            get { return _family; }
-            set { RaisePropertyChanged(ref _family, value); }
+            get => _family;
+            set => RaisePropertyChanged(ref _family, value);
         }
 
         private TextWrapping _wrap;
         public TextWrapping Wrap
         {
-            get { return _wrap; }
-            set
-            {
-                RaisePropertyChanged(ref _wrap, value);
-            }
+            get => _wrap;
+            set => RaisePropertyChanged(ref _wrap, value);
         }
 
         private bool _isWrapped;
         public bool IsWrapped
         {
-            get { return _isWrapped; }
+            get => _isWrapped;
             set
             {
                 RaisePropertyChanged(ref _isWrapped, value);
@@ -58,8 +55,8 @@ namespace Notepad2.Notepad
         private double _size;
         public double Size
         {
-            get { return _size; }
-            set { RaisePropertyChanged(ref _size, value); }
+            get => _size;
+            set => RaisePropertyChanged(ref _size, value);
         }
     }
 }
